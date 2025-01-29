bf6c0d7d8b46609b35551c2cc5577e9425e3f91a
Liezel Tumbaga <liezeltumbaga.03@gmail.com>
2025-01-29 19:27:25 +0800

    Added TODO CRUD Management project

---
+++ script.js
@@ -0,0 +1,62 @@
+// ==== Todo CRUD Management ====
+
+// Array too store todos
+let todos = [];
+
+//DOM Elements
+const todoForm =document.getElementById('todo-form');
+const tdoInput = document.getElementById('todo-input');
+const todolist = document.getElementById('todo-list');
+// Function to render todos
+
+function renderTodos() {
+    todolist.innerHTML = ''; // CLear the list 
+    todos.forEach((todo, index) => {
+        const li = document.createElement('li');
+        li.className = 'todo-item';
+        li.innerHTML = `  
+            <span>${todo}</span>
+            <button onclick="deleteTodo(${index})">Edit</button>
+            <button onlick="deleteTodo(${index})">Delete</button>
+        `;
+        todolist.appendChild(li);
+    });
+}
+
+// Function to add a new todo
+function addTodo(event){
+    event.preventDefault(); // Prevent form submission 
+    const newTodo = todoInput.value.trim();
+    if (newTodo) {
+        todos.push(newTodo);
+        todoInput.value = ''; // Clear the input
+        renderTodos();
+    }     
+}
+
+// Function to edit a todo 
+function editTodo(index) {
+    const updateTodo = prompt('Edit your todo:', todos[index]);
+    if (updateTodo !==null) {
+        todos[index] = updateTodo.trim();
+        renderTodos();
+    }
+}
+
+// Function to delete a todo
+function deleteTodo(index) {
+    if (confirm('Are you sure you want to delete this todo?')) {
+        todos.splice(index, 1);
+        renderTodos();
+    }
+}
+
+// Event Listeners
+todoForm.addEventListener('submit', addTodo);
+
+// Initial render 
+renderTodos();
+
+
+
+
